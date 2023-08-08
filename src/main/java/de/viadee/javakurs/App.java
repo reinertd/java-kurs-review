666124347ab982bbe2b1000e8a376e2662c50a79
Daniel Reinert <daniel.reinert@viadee.de>
2023-08-08 21:23:51 +0200

    Übung 2

--- src/main/java/de/viadee/javakurs/App.java
+++ src/main/java/de/viadee/javakurs/App.java
@@ -3,7 +3,7 @@ package de.viadee.javakurs;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hallo Welt!");
+        System.out.println("Hallo Bochum!");
     }
 
 }
