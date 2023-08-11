1f08012c915d2578f639c5e83452dbe5419d3025
Daniel <Daniel.Reinert@viadee.de>
2023-08-11 14:26:27 +0200

    Übung 7: Swing

--- src/main/java/de/viadee/javakurs/App.java
+++ src/main/java/de/viadee/javakurs/App.java
@@ -1,26 +1,40 @@
 package de.viadee.javakurs;
 
 import de.viadee.javakurs.services.UserService;
+import de.viadee.javakurs.view.LoginWindow;
+
+import javax.swing.*;
 
 public class App {
 
     public static String TITEL = "Diavolo 5";
 
+    private final JFrame mainWindow;
+
     public App() {
-        UserService userService = new UserService();
+        mainWindow = new JFrame(TITEL);
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainWindow.setIconImage(new ImageIcon(getClass().getResource("/pizza.png")).getImage());
 
-        String email = "Hallo Bochum!";
-        String mailOk = userService.validateEMail(email);
+        // Initialize Sub-Windows
+        this.loginWindow = new LoginWindow(new UserService());
 
-        if(mailOk.equals("")) {
-            System.out.println("E-Mail ist Ok!");
-        } else {
-            System.out.println("E-Mail ist nicht gut!");
-        }
+        // Switch to login
+        SwingUtilities.invokeLater(this::switchToLoginWindow);
     }
+
+    private final LoginWindow loginWindow;
 
     public static void main(String[] args) {
         new App();
     }
 
+    public void switchToLoginWindow() {
+        mainWindow.setContentPane(loginWindow);
+        mainWindow.pack();
+        mainWindow.setSize(330, 200);
+        mainWindow.setResizable(false);
+        mainWindow.setLocationRelativeTo(null);
+        mainWindow.setVisible(true);
+    }
 }
