7c4d5633c4d8158ebcf6a1cf8846deab47f2f3d1
Daniel <Daniel.Reinert@viadee.de>
2023-08-11 11:04:44 +0200

    Übung 5: E-Mail prüfen

--- src/main/java/de/viadee/javakurs/App.java
+++ src/main/java/de/viadee/javakurs/App.java
@@ -1,9 +1,18 @@
 package de.viadee.javakurs;
 
+import org.apache.commons.validator.EmailValidator;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hallo Bochum!");
+        String email = "Hallo Bochum!";
+        boolean mailOk = EmailValidator.getInstance().isValid(email);
+
+        if(mailOk) {
+            System.out.println("E-Mail ist Ok!");
+        } else {
+            System.out.println("E-Mail ist nicht gut!");
+        }
     }
 
 }
